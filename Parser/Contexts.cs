819c4b580e2447fb41a8b36c6d44c4374b1b0499
GlobalAstral <cristianmainenti.03@gmail.com>
2026-08-23 17:15:26 +0200

    Added Struct Context

--- Parser/Contexts.cs
+++ Parser/Contexts.cs
@@ -7,3 +7,8 @@ public class FunctionContext(DataType? retType, Variable[] arguments) : Context
   public DataType? ReturnType {get;} = retType;
   public Variable[] Arguments {get;} = arguments;
 }
+
+public class StructContext(Struct @struct) : Context
+{
+  public Struct Struct {get;} = @struct;
+}
