1b84fb7d0c44d1b279313dd00d249a876bbaaf8b
GlobalAstral <cristianmainenti.03@gmail.com>
2026-08-23 14:45:12 +0200

    Added ToString() to Variable

--- Parser/Variable.cs
+++ Parser/Variable.cs
@@ -1,3 +1,10 @@
 namespace Parser;
 
-public record Variable(ModifierHandler Modifiers, DataType Type, string Name) {}
+public class Variable(ModifierHandler Modifiers, DataType Type, string Name)
+{
+  public ModifierHandler Modifiers {get;} = Modifiers;
+  public DataType Type {get;} = Type;
+  public string Name {get;} = Name;
+
+  public override string ToString() => $"{Modifiers} {Type} {Name}";
+}
