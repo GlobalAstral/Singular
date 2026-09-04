6549837e98fde5adf4a71537a544fdd623455354
GlobalAstral <cristianmainenti.03@gmail.com>
2026-09-04 16:06:22 +0200

    Added formatter

--- Singular.cs
+++ Singular.cs
@@ -1,8 +1,33 @@
-﻿using Lexer;
+﻿using System.Diagnostics;
+using System.Reflection;
+using Lexer;
 using Parser;
 partial class Singular
 {
   public static readonly string SRC_EXT = ".sgl";
+
+  static string ExtractClangFormat()
+  {
+    string outputDir = Path.Combine(
+      "clang-format"
+    );
+
+    Directory.CreateDirectory(outputDir);
+
+    string outputPath = Path.Combine(outputDir, "clang-format.exe");
+
+    if (File.Exists(outputPath))
+      return outputPath;
+
+    Assembly assembly = Assembly.GetExecutingAssembly();
+
+    string resourceName = "Singular.clang_format.clang-format.exe";
+
+    using Stream? resource = assembly.GetManifestResourceStream(resourceName) ?? throw new Exception($"Could not find embedded resource {resourceName}");
+    using FileStream file = File.Create(outputPath);
+    resource.CopyTo(file);
+    return outputPath;
+  }
 
   static void Main(string[] args)
   {
@@ -28,13 +53,43 @@ partial class Singular
     Console.WriteLine("Statements:\n");
     foreach (Statement item in statements)
       Console.WriteLine(item);
-    
+
     Generator.Generator generator = new(statements, parser.GetHashCode());
     Console.WriteLine("Compiling...");
     string output = string.Join("\n", generator.Process());
     Console.WriteLine(output);
-    StreamWriter f = File.CreateText("test.c");
-    f.Write(output);
+
+    string outputfile = "test.c";
+
+    string clangFormat = ExtractClangFormat();
+    var info = new ProcessStartInfo
+    {
+      FileName = clangFormat,
+      RedirectStandardInput = true,
+      RedirectStandardOutput = true,
+      RedirectStandardError = true,
+      UseShellExecute = false,
+      CreateNoWindow = true
+    };
+
+    info.ArgumentList.Add("--style=LLVM");
+    info.ArgumentList.Add($"--assume-filename={outputfile}");
+
+    using Process process = Process.Start(info)!;
+
+    process.StandardInput.Write(output);
+    process.StandardInput.Close();
+
+    string formatted = process.StandardOutput.ReadToEnd();
+    string errors = process.StandardError.ReadToEnd();
+
+    process.WaitForExit();
+
+    if (process.ExitCode != 0)
+      throw new Exception(errors);
+
+    StreamWriter f = File.CreateText(outputfile);
+    f.Write(formatted);
     f.Close();
   }
 }
